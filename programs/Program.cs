c9d12b2e2acf0a5028685d1aa0e4c66a4143915f
Suleymanha <suleymanha@code.edu.az>
2021-10-12 04:13:45 +0400

    Add project files.

---
+++ programs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList programs = new SortedList();
+            programs.Add(2000 ,"C#");
+            programs.Add(1995,"JAVA" );
+            programs.Add(1998,"php" );
+
+            foreach(DictionaryEntry item in programs)
+            {
+                Console.WriteLine("{0}and{1}",item.Key,item.Value);
+
+            }
+            Console.WriteLine();
+            
+           
+        }
+    }
+}
---
+++ programs/ProgramLIst.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace programs
+{
+    class ProgramLIst
+    {
+        class Program<T>
+        {
+            private T[] arr;
+
+            public Program()
+            {
+                arr = new T[0];
+
+            }
+
+            public void Add(T item)
+            {
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length - 1] = item;
+
+            }
+            public T[] GetArray()
+            {
+
+                return arr;
+            }
+            public int Count
+            {
+                get
+                {
+                    return arr.Length;
+                }
+
+            }
+        }
+    }
+}
